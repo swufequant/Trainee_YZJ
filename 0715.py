f14ca7f694daf9575fd57ea6f953bf8d58f7345c
Yang1506 <you@example.com>
2020-07-16 10:29:57 +0800

    分别提取涨停信息中的代码日期和次数

---
+++ 0715.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 14:39:52 2020
+
+@author: 8611
+"""
+
+from MongoDBReader import MongoDBReader
+import os
+import csv
+import numpy as np
+
+def QueryUplimitCodeAndDate():
+    reader = MongoDBReader()
+    reader.login("")
+    filename = "all_info.csv"
+
+    with open(filename, 'rt') as file:
+        reader = csv.reader(file)
+        # code = [row[0] for row in reader]
+        # date_ = [row[1] for row in reader]
+        # uplimit_times = [row[2] for row in reader]
+        for i, row in enumerate(reader):
+           code, date, uplimit_times = row
+           print (code, date, uplimit_times)
+           if (i>6):
+           #if (i<-1):
+               break
+       
+           
+        
+        '''for i, row in enumerate(reader):
+            code, date, uplimit_times = row
+            print(code, date, uplimit_times)
+            if (i> 5):
+                break'''
+    # print (code)
+        
+  
+if __name__ == "__main__":
+    QueryUplimitCodeAndDate()
+   
