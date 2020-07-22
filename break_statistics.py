a0186614d0351b6349f02e598811659c701e8bc1
Yang1506 <you@example.com>
2020-07-22 17:06:16 +0800

    统计了在14:57之后破板的数目为3895

--- break_statistics.py
+++ break_statistics.py
@@ -5,24 +5,49 @@ from MongoDBReader import MongoDBReader
 import csv
 import os
 import time
-import datetime
+from datetime import datetime
+import re
 
 def BreakStatistics():
-    filename = "Rate_Info.csv"
+    reader = MongoDBReader()
+    reader.login("")
+    filename = "Rate_Info.csv"    
     df = pd.read_csv(filename)
-
     date = df['data']
-    break_time = df['break_time']
+    df['break_time'] = pd.to_datetime(df['break_time'])
+    #print (type(df['break_time']))
+    #dt = re.findall(r'^\w + \s(.*?)$',db)
+    #print (df.info())#dt = datetime(2015,4,19,12,20)
+    df['time'] = pd.to_datetime(df['data'] + ' ' + '14:57:00.000')
+    #print(df.info())
+    print(df[df["break_time"] > df['time']].count())#个数为3895
+'''    with open(filename, 'rt') as file:
+        reader_ = csv.reader(file)
+        All_Info = []
+        for i, row in enumerate(reader_):
 
+            date = df['data']
+            df['break_time'] = pd.to_datetime(df['break_time'])
+            #print (type(df['break_time']))
+            #dt = re.findall(r'^\w + \s(.*?)$',db)
+            #print (df.info())#dt = datetime(2015,4,19,12,20)
+            df['time'] = pd.to_datetime(df['data'] + ' ' + '14:57:00.000')
+            #print(df.info())
+            j=0
+            if (i<-1):          
+                if df['break_time'].__ge__(df['time']) is False:
+                    j = j+1
+                break
+                print (j)
+'''    
+
+    #print(type(dt))
+
+ 
     #print(date)
     #print (type(break_time))
-    #dtime_st = datetime.datetime.strptime('time_st','%H:%M:%S')
- 
-    time_st = date + ' ' + '14:57:00'
-
-    diff = time_st - break_time
-    #print(diff)
-    
+   
+    #print(diff)  
     
 if __name__ == "__main__":
     BreakStatistics()
